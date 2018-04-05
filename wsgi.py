ec037f312d7aaec43c788d070e95e6206f5cf6f3
ShiinaOrez <shiina_orez@qq.com>
2018-04-06 00:22:13 +0800

    xxx

---
+++ wsgi.py
@@ -0,0 +1,9 @@
+from manage import app
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=1477)
+
+from manage import app 
+
+if __name__=='__main__':
+    app,run(host='0.0.0.0',port=1477)
